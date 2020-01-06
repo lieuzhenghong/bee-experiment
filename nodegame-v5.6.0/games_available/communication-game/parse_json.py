adf6dda0fc05279564b009e65f3df3296d416e0b
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2020-01-06 16:04:01 +0800

    Add treatment condition to parse_json.py

--- nodegame-v5.6.0/games_available/communication-game/parse_json.py
+++ nodegame-v5.6.0/games_available/communication-game/parse_json.py
@@ -1,12 +1,11 @@
 import json
 
+#[TODO] write error correction code to handle cases where there's a disconnect
+
 def parse_data_dump(filename):
 
     with open(filename) as f:
         d = json.load(f)
-
-        #print(d)
-
         parsed = {}
 
         parsed['filename'] = filename
@@ -19,10 +18,19 @@ def parse_data_dump(filename):
         parsed['p1_msg'] = strip_msg_form(p1_msg)
         parsed['p2_msg'] = strip_msg_form(p2_msg)
 
+        if len(parsed['p1_msg']) == 4:
+            parsed['treatment'] = 'prosocial'
+        elif len(parsed['p1_msg']) == 3: 
+            parsed['treatment'] = 'shared_knowledge'
+        elif len(parsed['p1_msg']) == 2: 
+            parsed['treatment'] = 'imperative'
+        elif len(parsed['p1_msg']) == 1: 
+            parsed['treatment'] = 'intent'
+        else:
+            pass # TODO throw error here
+
         parsed['p1_choice'] = d[8]['forms']['stag_choice']['choice']
         parsed['p2_choice'] = d[9]['forms']['stag_choice']['choice']
-
-        print(parsed)
 
         with open(filename.split('.json')[0] + '_parsed.json', 'w', encoding='utf-8') as f:
             json.dump(parsed, f, ensure_ascii=False, indent=4)
@@ -30,4 +38,4 @@ def parse_data_dump(filename):
 def strip_msg_form(forms):
     return [int(v['choice']) for k,v in forms.items()]
 
-parse_data_dump('./data/room000122data.json')
+# parse_data_dump('./data/room000122data.json')
